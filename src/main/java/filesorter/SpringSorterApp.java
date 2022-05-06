f61e74d8fab6a3d30bf44610a02b456d5dd16a36
nvpetrov <nvpetrov@phoenixit.ru>
2022-05-06 23:44:10 +0300

    Initial commit

--- src/main/java/filesorter/SpringSorterApp.java
+++ src/main/java/filesorter/SpringSorterApp.java
@@ -1,7 +1,59 @@
 package filesorter;
 
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.common.ImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.tiff.TiffField;
+import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+import org.apache.commons.imaging.formats.tiff.constants.GpsTagConstants;
+import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
+import org.apache.commons.imaging.formats.tiff.taginfos.TagInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class SpringSorterApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ImageReadException {
+
+
+         File file = new File("/home/administrator/testDirectory/IMG_4078.JPG");
+        final ImageMetadata metadata = Imaging.getMetadata(file);
+
+        System.out.println(metadata.getItems());
+        if (metadata instanceof JpegImageMetadata) {
+            final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+
+            // Jpeg EXIF metadata is stored in a TIFF-based directory structure
+            // and is identified with TIFF tags.
+            // Here we look for the "x resolution" tag, but
+            //   we could just as easily search for any other tag.
+            //
+            // see the TiffConstants file for a list of TIFF tags.
+
+            System.out.println("file: " + file.getPath());
+
+            // print out various interesting EXIF tags.
+            printTagValue(jpegMetadata, TiffTagConstants.TIFF_TAG_XRESOLUTION);
+            printTagValue(jpegMetadata, TiffTagConstants.TIFF_TAG_DATE_TIME);
+            printTagValue(jpegMetadata,
+                    ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL);
+            printTagValue(jpegMetadata, ExifTagConstants.EXIF_TAG_DATE_TIME_DIGITIZED);
+            printTagValue(jpegMetadata, ExifTagConstants.EXIF_TAG_ISO);
+        }
+
+    }
+
+    private static void printTagValue(final JpegImageMetadata jpegMetadata,
+                                      final TagInfo tagInfo) {
+        final TiffField field = jpegMetadata.findEXIFValueWithExactMatch(tagInfo);
+        if (field == null) {
+            System.out.println(tagInfo.name + ": " + "Not Found.");
+        } else {
+            System.out.println(tagInfo.name + ": "
+                    + field.getValueDescription());
+        }
     }
 }
